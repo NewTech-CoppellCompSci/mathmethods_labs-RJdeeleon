7d6540c319e55def6a78e380a6f0c92d7eaca92c
rjd4055 <89941713+rjd4055@users.noreply.github.com>
2022-03-23 08:57:01 -0500

    Update PerformanceTask.java

--- string_methods_labs-RJdeeleon-master/src/labs/PerformanceTask.java
+++ string_methods_labs-RJdeeleon-master/src/labs/PerformanceTask.java
@@ -1,14 +1,62 @@
 package labs;
 
+import java.util.Scanner;
+
 public class PerformanceTask {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Type in a Password: ");
+		String password = inKey.next();
+		int length = password.length();
+			String numb[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+			String specC[] = {"!","@","#","$","%","^","&","*","?"};
+	String OspecC[]  = {",",".",";","/","'","{","[","]","}","\"","-","+","=",")","&","`", "~","<",">",};
+			boolean uppercase = false, lowercase = false, longs = false, shorts = false, specChar = false, num = false, pv = false;
+			if (password.length()>20) {
+			longs = true;
+			System.out.println("Password is too long");
+			}
+			if (password.length()<8) {
+		shorts = true;
+		System.out.println("Password is too short");
+			}
+			for (int i = 0; i < length; i++) {
+		if(Character.isUpperCase(password.charAt(i))) {
+					uppercase = true;
+				}
+		if (Character.isLowerCase(password.charAt(i))) {
+					lowercase = true;
+				}
+			}
+			for (int i = 0; i < length; i++) {
+				for (int j = 0; j< numb.length; j++) {
+					if (password.substring(i, i+1).equals(numb[j])) {
+						num = true;
+					}
+				}
+			}
+			for (int i = 0; i < length; i++) {
+				for (int j = 0; j< specC.length; j++) {
+					if (password.substring(i, i+1).equals(specC[j])) {
+						specChar = true;
+					}
+				}
+			}
+			for (int i = 0; i < length; i++) {
+				for (int j = 0; j< OspecC.length; j++) {
+			if (password.substring(i, i+1).equals(OspecC[j])) {
+					System.out.println("Special Character not recognized: " + password.substring(i));
+						break;
+					}
+				}
+			}
+			if (uppercase == true && lowercase == true && specChar == true && num == true) {
+				System.out.println("Password is Valid!");
+		pv = true;
+			}
+			else {
+				System.out.println("Password does not meet requirements");
+			}
+		}
 	}
-	
-}
